9f9989655dda6fc1dd0325ad8cfdfb9c3ea94902
Vijay <jangravijay27@gmail.com>
2023-01-15 18:58:36 +0530

    changes

--- src/components/Placeholder.js
+++ src/components/Placeholder.js
@@ -17,7 +17,6 @@ const placeholder = () => {
       <span class="placeholder col-6"></span>
       <span class="placeholder col-8"></span>
     </p>
-    <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
   </div>
 </div>    
     </>
